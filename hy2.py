decaf981203e4b51da17dfa7be45fb546e0c50c5
ironicone <101384492+ironicone@users.noreply.github.com>
2023-10-18 14:51:20 +0800

    Update hy2.py
    加入主函数

--- hy2.py
+++ hy2.py
@@ -1,25 +1,30 @@
 import requests
 import json
 
-# 下载JSON配置文件
-url = "https://gitlab.com/free9999/ipupdate/-/raw/master/hysteria2/config.json"
-response = requests.get(url)
-if response.status_code == 200:
-    config_data = json.loads(response.text)
-    
-    # 生成新的内容
-    auth = config_data.get("auth", "")
-    server = config_data.get("server", "")
-    tls = config_data.get("tls", {})
-    insecure = tls.get("insecure", False)
-    sni = tls.get("sni", "")
-    
-    new_content = f"hy2://{auth}@{server}/?insecure={int(insecure)}&sni={sni}"
+def generate_hy2_config():
+    # 下载JSON配置文件
+    url = "https://gitlab.com/free9999/ipupdate/-/raw/master/hysteria2/config.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        config_data = json.loads(response.text)
+        
+        # 生成新的内容
+        auth = config_data.get("auth", "")
+        server = config_data.get("server", "")
+        tls = config_data.get("tls", {})
+        insecure = tls.get("insecure", False)
+        sni = tls.get("sni", "")
+        
+        new_content = f"hy2://{auth}@{server}/?insecure={int(insecure)}&sni={sni}"
 
-    # 将新内容保存到txt文件
-    with open("hy2.txt", "w", encoding="utf-8") as f:
-        f.write(new_content)
+        # 将新内容保存到txt文件
+        with open("hy2.txt", "w", encoding="utf-8") as f:
+            f.write(new_content)
 
-    print("新内容已保存到hy2.txt")
-else:
-    print(f"请求失败，状态码：{response.status_code}")
+        print("新内容已保存到hy2.txt")
+    else:
+        print(f"请求失败，状态码：{response.status_code}")
+
+if __name__ == "__main__":
+    generate_hy2_config()
+    
