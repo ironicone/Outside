a7a7bb529046653f519d90686c1d3d94ce45aae7
ironicone <101384492+ironicone@users.noreply.github.com>
2023-10-28 23:33:20 +0800

    Update hy2.py

--- hy2.py
+++ hy2.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import base64
 
 # URL列表
 urls = [
@@ -10,9 +9,10 @@ urls = [
     "https://www.githubip.xyz/Alvin9999/pac2/master/hysteria2/2/config.json"
 ]
 
-# 创建一个空的列表，用于存储所有内容
+# 创建三个空的列表，用于存储所有内容
 all_content_original = []
 all_content_new = []
+all_json_data = []
 
 for url in urls:
     response = requests.get(url)
@@ -32,16 +32,24 @@ for url in urls:
         new_content = f"hysteria2://{auth}@{server}/?peer={sni}&insecure={int(insecure)}&obfs=none&fastopen=1"
         all_content_new.append(new_content)
 
+        # 将JSON数据按原始格式添加到列表
+        json_str = json.dumps(config_data, indent=2)
+        all_json_data.append(json_str)
+
 print("所有内容已生成")
 
-# 将所有内容保存到两个不同的txt文件并进行base64编码
+# 将原始JSON数据保存到txt文件
+with open("hy2_json.txt", "w", encoding="utf-8") as f:
+    for json_data in all_json_data:
+        f.write(json_data + "\n")
+
+# 将所有内容保存到一个单独的txt文件
 with open("hy2.txt", "w", encoding="utf-8") as f:
     for content in all_content_original:
         f.write(content + "\n")
 
-with open("hy2_base64.txt", "w", encoding="utf-8") as f:
+with open("hy2_rocket.txt", "w", encoding="utf-8") as f:
     for content in all_content_new:
-        content_base64 = base64.b64encode(content.encode('utf-8')).decode('utf-8')
-        f.write(content_base64 + "\n")
+        f.write(content + "\n")
 
 print("所有内容已保存到文件")
